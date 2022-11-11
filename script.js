4e2366e80df624f5cfa789607c1076fc186847eb
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2022-11-11 22:00:37 +0400

    starting account-menu

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let burger_menu = function(){
+let burger_menu = function () {
     const burgerBtn = document.getElementById('menuBtn');
     const menu = document.getElementById('menu');
     burgerBtn.addEventListener('click', function (e) {
@@ -8,3 +8,12 @@ let burger_menu = function(){
 }
 burger_menu();
 
+let account_menu = function () {
+    const accountBtn = document.getElementById('accountBtn');
+    const menu = document.getElementById('account-menu');
+    accountBtn.addEventListener('click', function (e) {
+        menu.classList.toggle('active');
+        accountBtn.classList.toggle('active');
+    });
+}
+burger_menu();
