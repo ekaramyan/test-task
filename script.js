92de758220013e415bf4fa5ec5c9b74862f3d484
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2022-11-12 00:01:31 +0400

    making account menu, have issue with account__btn

--- script.js
+++ script.js
@@ -10,10 +10,12 @@ burger_menu();
 
 let account_menu = function () {
     const accountBtn = document.getElementById('accountBtn');
-    const menu = document.getElementById('account-menu');
+    const menu = document.getElementById('accountMenu');
+    const arrow = document.getElementById('accountArrow')
     accountBtn.addEventListener('click', function (e) {
         menu.classList.toggle('active');
         accountBtn.classList.toggle('active');
+        arrow.classList.toggle('active');
     });
 }
-burger_menu();
+account_menu();
